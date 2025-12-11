bd7bed4477419481d45d21e3cca423d3aeda7b72
Ritik Gupta <ritik1262003@gmail.com>
2025-12-11 17:10:22 +0530

    Batch Predication

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import certifi
+
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 ca = certifi.where()
 
 from dotenv import load_dotenv
@@ -36,7 +38,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", tags=["authentication"])
 async def index():
@@ -52,6 +55,28 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/best_model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
 
 # OPTIONAL MAIN RUNNER (KEEP ONLY IF YOU HAD THIS)
 if __name__ == "__main__":
